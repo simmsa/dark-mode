84b4346d376d71313956d6f24c8ec9d52b28632f
Andrew Simms <simms.andrew@gmail.com>
2015-04-10 10:57:09 -0600

    Add dark-mode.js

---
+++ dark-mode.js
@@ -0,0 +1,11 @@
+// function toggle_dark_mode(mode){
+//     document.documentElement.setAttribute('dark-mode', mode);
+// }
+
+// function main(){
+//     toggle_dark_mode("on");
+//     chrome.extension.onRequest.addListener(inject_dark_mode);
+//     chrome.extension.sendRequest({'init': true}, inject_dark_mode);
+// }
+
+// main();
