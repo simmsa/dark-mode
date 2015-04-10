154d999baf1e9971b7f02e69acc203b0a00176dd
Andrew Simms <simms.andrew@gmail.com>
2015-04-10 10:55:27 -0600

    Add background.js.

---
+++ background.js
@@ -0,0 +1,30 @@
+// Listen for an event / one-time request from the popup
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.type){
+        case "toggle-dark-mode":
+            // alert("recieved request in background.");
+            activateDarkMode();
+            break;
+    }
+    return true;
+});
+
+// Listen for an event / long-lived connections coming
+// from devtools
+chrome.extension.onConnect.addListener(function(port){
+    port.onMessage.addListener(function(message){
+        switch(port.name){
+            case "toggle-dark-mode-port":
+                activateDarkMode();
+                break;
+        }
+    });
+});
+
+// Send a message to the content script
+var activateDarkMode = function(){
+    chrome.tabs.getSelected(null, function(tab){
+        // alert("sending request from background.");
+        chrome.tabs.sendMessage(tab.id, {type: "toggle-dark-mode"});
+    });
+}
