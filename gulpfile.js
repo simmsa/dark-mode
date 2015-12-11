9d6d692fd0d0bc6d83cfc30399d37ca01c4d60d4
Andrew Simms <simms.andrew@gmail.com>
2015-12-10 21:19:52 -0700

    Feature: Migrate from css to less.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var less = require('gulp-less');
 
 gulp.task('default', function(){
     // Do stuff here
 });
 
 gulp.task('file-watch', function(){
-
     watch('**/*.*', function(file){
         // Do things when files change here
+        gulp.src("styles/less/*.less")
+        .pipe(less({
+            paths: []
+        }))
+        .pipe(gulp.dest("styles/css"));
     });
 });
