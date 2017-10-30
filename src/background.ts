4044140d6653a3ab86fdfce9009aeb8cd88519a9
Andrew Simms <simms.andrew@gmail.com>
2017-10-30 13:06:26 -0600

    Lint: Remove unnecessary quotes around object accessors

--- src/background.ts
+++ src/background.ts
@@ -1857,8 +1857,8 @@ class Icon {
   public static turnOn() {
     chrome.browserAction.setIcon({
       path: {
-        "19": "img/dark-mode-on-19.png",
-        "38": "img/dark-mode-on-38.png",
+        19: "img/dark-mode-on-19.png",
+        38: "img/dark-mode-on-38.png",
       },
     });
   }
@@ -1866,8 +1866,8 @@ class Icon {
   public static turnOff() {
     chrome.browserAction.setIcon({
       path: {
-        "19": "img/dark-mode-off-19.png",
-        "38": "img/dark-mode-off-38.png",
+        19: "img/dark-mode-off-19.png",
+        38: "img/dark-mode-off-38.png",
       },
     });
   }
