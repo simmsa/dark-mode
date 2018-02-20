38764ca94292f970b89c7966302d7ec693697d2a
Andrew Simms <simms.andrew@gmail.com>
2018-02-20 14:56:34 -0700

    Test: add `ParseIntBase10` utility

--- ___tests___/utils-test.ts
+++ ___tests___/utils-test.ts
@@ -1,5 +1,7 @@
 import * as utils from "../src/utils";
 
+global.console = { ...global.console, trace: jest.fn() };
+
 describe("`capitalize` utility", () => {
   const testCapitalize = (input: string, expected: string) => {
     expect(utils.capitalize(input)).toBe(expected);
@@ -15,5 +17,17 @@ describe("`capitalize` utility", () => {
 
   test("Handles spaces correctly", () => {
     testCapitalize(" word", " word");
+  });
+});
+
+describe("`ParseIntBase10` utility", () => {
+  test("Parses a number correctly", () => {
+    // tslint:disable:no-magic-numbers
+    expect(utils.ParseIntBase10("10")).toBe(10);
+  });
+
+  test("Logs an error when passed an invalid string", () => {
+    expect(utils.ParseIntBase10("test")).toBe(0);
+    expect(console.trace).toBeCalled();
   });
 });
